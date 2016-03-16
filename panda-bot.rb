8fc33c7b01fc3305c53259913995efe66654d5ae
Vitaliy <croaton6@gmail.com>
2016-03-16 19:54:56 +0300

    fix weather

--- panda-bot.rb
+++ panda-bot.rb
@@ -1,3 +1,4 @@
 require 'slack-ruby-bot'
 require 'panda-bot/commands/calculate'
+require 'panda-bot/commands/weather'
 require 'panda-bot/bot'
--- panda-bot/commands/weather.rb
+++ panda-bot/commands/weather.rb
@@ -2,19 +2,13 @@ module PandaBot
   module Commands
     class Weather < SlackRubyBot::Commands::Base
       command 'weather' do |client, data, _match|
-        response = get_response
-        get_desc(response)
-      end
-
-      def get_response
-        HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Saint+Petersburg&APPID=01e22e4baba3b929e246851ed8564664&units=metric")
-      end
-
-      def get_desc(response)
+        response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Saint+Petersburg&APPID=01e22e4baba3b929e246851ed8564664&units=metric")
         desc = response['weather'][0]['description']
         temp = response['main']['temp']
         wind = response['wind']['speed']
-        "#{desc.capitalize}, #{temp}&deg, #{wind}m/s"
+        result = "#{desc.capitalize}, #{temp}C, #{wind}m/s"
+
+        client.say(channel: data.channel, text: result)
       end
     end
   end
